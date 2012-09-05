c6de6826dd60da8c71fa0ac6339ba205e50a1817
giginet <giginet.net@gmail.com>
2012-09-05 23:00:41 +0900

    バージョン情報の追加

--- dqx_helper/__init__.py
+++ dqx_helper/__init__.py
@@ -1,5 +1,7 @@
 BASE_URL = r'http://hiroba.dqx.jp'
+VERSION = '1.0.0'
 from auth import Auth
 from bazaar import Bazaar
 from character import Character
 from photo import Photo
+from team import Team
